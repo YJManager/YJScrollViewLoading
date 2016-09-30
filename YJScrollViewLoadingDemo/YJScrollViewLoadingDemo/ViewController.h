c7b529f3cd51cd5769041e42fbca12393a11c97a
YJHou <8069060@qq.com>
2016-09-30 15:23:18 +0800

    初始化YJScrollViewLoading的工程

---
+++ YJScrollViewLoadingDemo/YJScrollViewLoadingDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YJScrollViewLoadingDemo
+//
+//  Created by YJHou on 16/9/30.
+//  Copyright © 2016年 YJHou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
