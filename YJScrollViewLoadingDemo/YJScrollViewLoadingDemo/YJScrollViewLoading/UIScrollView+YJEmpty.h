d2607c6aff6f67e3a36e7fa311658ceeea9349cd
YJHou <8069060@qq.com>
2016-10-08 16:30:46 +0800

    设置

---
+++ YJScrollViewLoadingDemo/YJScrollViewLoadingDemo/YJScrollViewLoading/UIScrollView+YJEmpty.h
@@ -0,0 +1,13 @@
+//
+//  UIScrollView+YJEmpty.h
+//  YJScrollViewLoadingDemo
+//
+//  Created by YJHou on 16/10/8.
+//  Copyright © 2016年 YJHou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIScrollView (YJEmpty)
+
+@end
---
+++ YJScrollViewLoadingDemo/YJScrollViewLoadingDemo/YJScrollViewLoading/UIScrollView+YJEmpty.m
@@ -0,0 +1,13 @@
+//
+//  UIScrollView+YJEmpty.m
+//  YJScrollViewLoadingDemo
+//
+//  Created by YJHou on 16/10/8.
+//  Copyright © 2016年 YJHou. All rights reserved.
+//
+
+#import "UIScrollView+YJEmpty.h"
+
+@implementation UIScrollView (YJEmpty)
+
+@end
